c9bff63e4f495896fc3167a1db8af0613df02091
Tobias Rice <tobias@fatofthelan.com>
2018-06-20 21:21:45 -0700

    Cleaned up outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,28 @@
+output "FW1_Tunnel_1_Pre_Shared_Key" {
+  value = "${aws_vpn_connection.spoke_to_transit_fw1.tunnel1_preshared_key}"
+}
+
+output "FW1_Tunnel_2_Pre_Shared_Key" {
+  value = "${aws_vpn_connection.spoke_to_transit_fw1.tunnel2_preshared_key}"
+}
+
 output "Firewall 1 Management URL" {
   value = "${join("", list("https://", "${aws_eip.firewall_1_management_public_ip.public_ip}"))}"
+}
+
+output "FW2_Tunnel_1_Pre_Shared_Key" {
+  value = "${aws_vpn_connection.spoke_to_transit_fw2.tunnel1_preshared_key}"
+}
+
+output "FW2_Tunnel_2_Pre_Shared_Key" {
+  value = "${aws_vpn_connection.spoke_to_transit_fw2.tunnel2_preshared_key}"
 }
 
 output "Firewall 2 Management URL" {
   value = "${join("", list("https://", "${aws_eip.firewall_2_management_public_ip.public_ip}"))}"
 }
+
+/* Debugging Outputs - Uncomment if needed.
 
 output "Firewall 1 Management EIP" {
   value = "${aws_eip.firewall_1_management_public_ip.public_ip}"
@@ -45,11 +63,5 @@ output "Tunnel 1 Virtual Gateway Outside IP (FW1-ike-gw1-peer-address)" {
 output "Tunnel 2 Virtual Gateway Outside IP (FW1-ike-gw2-peer-address)" {
   value = "${aws_vpn_connection.spoke_to_transit_fw2.tunnel2_address}"
 }
+*/
 
-output "Tunnel 1 Pre-Shared Key" {
-  value = "${aws_vpn_connection.spoke_to_transit_fw1.tunnel1_preshared_key}"
-}
-
-output "Tunnel 2 Pre-Shared Key" {
-  value = "${aws_vpn_connection.spoke_to_transit_fw1.tunnel2_preshared_key}"
-}
